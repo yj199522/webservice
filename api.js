8dc348201aebdca36ac469d7c0a7818b845776fd
yj199522 <jain.yash3@northeastern.edu>
2022-02-16 13:54:42 -0500

    Added API endpoint

--- api.js
+++ api.js
@@ -1,25 +1,30 @@
 const express = require('express');
 const app = express();
+const {
+    createUser,
+    updateUser,
+    viewUser
+} = require("./controllers");
 
 
 const cors = require("cors");
-const pool = require('./db');
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
 //Routes
+app.post("/v1/user", createUser);
+app.put("/v1/user/self", updateUser);
+app.get("/v1/user/self", viewUser);
 app.get("/healthz", (req, res) => {
     try {
         res.status(200).json("server responds with 200 OK if it is healhty.")
     } catch (err) {
         res.json(err.message);
     }
-})
-
-app.get('*', function(req, res){
+});
+app.get('*', function (req, res) {
     res.status(404).json("Page not found!")
 });
-
 module.exports = app;
--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const app = require("./api")
+const app = require("./api");
+const pool = require('./db');
 
 
 app.listen(2000, () => {
     console.log("Server has started on port 2000");
 })
+
+// connecting DataBase
+
+pool.connect((err) => {
+    if (err) throw err;
+    pool.query('create table if not exists public.users(id UUID NOT NULL,username VARCHAR(100),password VARCHAR(100),first_name VARCHAR(50),last_name VARCHAR(50),account_created timestamp with time zone,account_updated timestamp with time zone, PRIMARY KEY (id));',
+        function (error, result) {
+            console.log(result);
+        });
+});
