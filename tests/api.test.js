14467b9c60a010a6281956b1da342339ee1de508
yj199522 <jain.yash3@northeastern.edu>
2022-03-30 13:18:46 -0400

    test codedeploy

--- tests/api.test.js
+++ tests/api.test.js
@@ -4,7 +4,7 @@ const request = require("supertest");
 describe("GET /healthz", () => {
     test("Testing GET API", async() => {
         const response = await request(app).get("/healthz");
-        expect(response.body).toEqual("server responds with 200 OK if it is healhty.");
+        expect(response.body).toEqual("OK");
         expect(response.statusCode).toBe(200);
     });
 });
