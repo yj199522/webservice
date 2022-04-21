ff6d7f8c304440d834e9ce01c0ae7fb81bbc44ba
yj199522 <jain.yash3@northeastern.edu>
2022-04-21 09:39:41 -0400

    Added ssl in postgres

--- db.js
+++ db.js
@@ -1,11 +1,17 @@
-const Pool = require("pg").Pool
+const Pool = require("pg").Pool;
+const fs = require('fs');
+const rdsCa = fs.readFileSync('./rds-combined-ca-bundle.pem');
 
 const pool = new Pool({
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOSTNAME,
     port: process.env.PORT,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: true,
+        ca: [rdsCa]
+    }
 })
 
 module.exports = pool;
